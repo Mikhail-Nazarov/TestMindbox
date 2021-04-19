65b62b152f5bfe92aa52a99aceb0fde398d97d23
Mikhail-Nazarov <misha-nazarov.nazarov@yandex.ru>
2021-04-19 12:38:44 +0700

    sq запрос

--- database.sql
+++ database.sql
@@ -1,30 +1,44 @@
 CREATE TABLE Products(
 	Id INT  PRIMARY KEY,
-	"Name" VARCHAR(255) NOT NULL
+	Name VARCHAR(255) NOT NULL
 );
 
 INSERT INTO Products
 VALUES
-	(1, 'Гарри Поттер и кубок огня'),
-	(2, 'Гарфилд'),
-	(3, 'Дэдпул'),
-	(4, 'Пила');
+	(1, 'Р“Р°СЂСЂРё РџРѕС‚С‚РµСЂ Рё РєСѓР±РѕРє РѕРіРЅСЏ'),
+	(2, 'Р“Р°СЂС„РёР»Рґ'),
+	(3, 'Р”СЌРґРїСѓР»'),
+	(4, 'РџРёР»Р° 6');
 
-CREATE TABLE Ctegories(
+CREATE TABLE Categories(
 	Id INT  PRIMARY KEY,
-	"Name" VARCHAR(255) NOT NULL
+	Name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO Products
+INSERT INTO Categories
 VALUES
-	(1, 'Фэнтези'),
-	(2, 'Комедия'),
-	(3, 'Боевик');
+	(1, 'Р¤СЌРЅС‚РµР·Рё'),
+	(2, 'РљРѕРјРµРґРёСЏ'),
+	(3, 'Р‘РѕРµРІРёРє');
 
-CREATE TABLE ProductsCtegories(
-	ProductId INT FOREIGN KEY REFERENCES Products(Id) NOT NULL,
-	CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+CREATE TABLE ProductsCategories(
+    ProductId INT,
+    CategoryId INT,
+	FOREIGN KEY (ProductId)  REFERENCES Products (Id),
+	FOREIGN KEY (CategoryId)  REFERENCES Categories (Id),
 	PRIMARY KEY (ProductId, CategoryId)
 );
 
+INSERT INTO ProductsCategories
+VALUES
+    (1,1),
+    (2,2),
+    (3,2),
+    (3,3);
 
+SELECT Products.Name, Categories.Name
+FROM Products
+LEFT JOIN ProductsCategories
+	ON Products.Id = ProductsCategories.ProductId
+LEFT JOIN Categories
+	ON ProductsCategories.CategoryId = Categories.Id;
